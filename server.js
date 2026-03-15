8c1d41a3b0754450ddbca09852a729b12164c04c
eg61ejub <wolfgang.roedle@gmail.com>
2026-03-15 21:07:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,4 +29,16 @@ io.on("connection", socket => {
 
   })
 
+socket.on("reset", () => {
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      board[y][x] = 0
+    }
+  }
+
+  io.emit("reset")
+
+})
+  
 })
