0b5a9c5d269e18c515e6d716ac779783810455a5
eg61ejub <wolfgang.roedle@gmail.com>
2026-03-15 21:00:57 +0100

    Spielstand-Speicher server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// --- Array für Spielstand-Speicherung
+const size = 19
+
+let board = []
+
+for (let y = 0; y < size; y++) {
+
+  board[y] = []
+
+  for (let x = 0; x < size; x++) {
+    board[y][x] = 0
+  }
+
+}
+
+
+// --- Server connecten
 io.on("connection", socket => {
 
   console.log("Player connected")
