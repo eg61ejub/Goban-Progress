77476ea6b3a6ac0871e28668c180873d2781f9fa
eg61ejub <wolfgang.roedle@gmail.com>
2026-03-15 20:42:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-const express = require("express")
-const app = express()
-
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
-
-app.use(express.static("public"))
-
 io.on("connection", socket => {
 
-  console.log("player connected")
+  console.log("Player connected")
 
   socket.on("move", data => {
-    socket.broadcast.emit("move", data)
+
+    io.emit("move", data)
+
   })
 
-})
-
-const port = process.env.PORT || 3000
-
-http.listen(port, () => {
-  console.log("server running")
 })
