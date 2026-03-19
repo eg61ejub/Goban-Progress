0e89a993c86cf8935ba73ac3aa629385646ee07d
eg61ejub <wolfgang.roedle@gmail.com>
2026-03-19 20:11:31 +0100

    Spielstand und Spieler speichern game.js

--- public/game.js
+++ public/game.js
@@ -1,3 +1,65 @@
+const socket = io();
+
+let myName = null;
+let myRole = null;
+let gameState = null;
+
+// --- Spiel beitreten
+function joinGame() {
+  const input = document.getElementById("nameInput");
+  myName = input.value;
+
+  socket.emit("joinGame", myName);
+}
+// --- Init vom Server epmfangen
+socket.on("init", (data) => {
+  myRole = data.role;
+  gameState = data.gameState;
+
+  console.log("Ich bin:", myRole);
+
+  drawBoard();
+});
+
+// --- Board zeichnen
+function drawBoard() {
+  clearBoard();
+
+  gameState.board.forEach(stone => {
+    drawStone(stone.x, stone.y, stone.color);
+  });
+}
+
+// --- Zug an Server senden
+canvas.addEventListener("click", (e) => {
+
+  if (!myRole || myRole === "spectator") return;
+
+  if (myRole !== gameState.currentTurn) {
+    console.log("Nicht dein Zug");
+    return;
+  }
+
+  const { x, y } = getBoardPosition(e);
+
+  socket.emit("makeMove", {
+    name: myName,
+    x,
+    y
+  });
+});
+
+// --- Updates empfangen
+socket.on("update", (newState) => {
+  gameState = newState;
+  drawBoard();
+});
+
+
+// -----------------------
+
+
+
 let socket = io()
 
 let canvas = document.getElementById("board")
